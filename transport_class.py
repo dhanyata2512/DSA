d798c55232aac874463df044843ce4ea47125335
D.P <dhanyata2512@gmail.com>
2025-05-13 18:05:54 +0100

    Add files via upload

---
+++ transport_class.py
@@ -0,0 +1,28 @@
+# creating a class
+
+class Transport:
+  
+  type="Water Vechicle"
+
+  def __init__(self, color,name,brand):
+    self.color=color
+    self.name=name
+    self.brand=brand
+
+
+Vechicle1=Transport("blue","boat","Malibu")
+print(Vechicle1.type)
+print(Vechicle1.color)
+print(Vechicle1.name)
+print(Vechicle1.brand)
+
+
+Vechicle2=Transport("violet","yacht","Feadship")
+print(Vechicle2.type)
+print(Vechicle2.color)
+print(Vechicle2.name)
+print(Vechicle2.brand)
+
+
+
+
