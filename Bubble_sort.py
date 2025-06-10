0f0b18c75ab26215a40b710c9dd10c0ebb4f83e9
D.P <dhanyata2512@gmail.com>
2025-06-10 18:26:56 +0100

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,8 +1,17 @@
 surname=['sharma','wong','patel','gautam','hart']
 
 length=len(surname)
-for p in range(length-1):
+for p in range(length):
     swapped=False
+    print('pass=',p)
     for s in range(length-1):
         if surname[s]>surname[s+1]:
             swapped=True
+            surname[s],surname[s+1]=surname[s+1],surname[s]
+            # temp=surname[s]
+            # surname[s]=surname[s+1]
+            # surname[s+1]=temp
+            print(surname)
+    if not swapped:
+        break
+    print(surname)
