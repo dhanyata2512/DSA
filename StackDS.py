f5b90957e183b6e7fe83bc86c50902933c9b351a
D.P <dhanyata2512@gmail.com>
2025-07-01 18:49:41 +0100

    Update StackDS.py

--- StackDS.py
+++ StackDS.py
@@ -4,8 +4,45 @@ class Stack:
         self.stack=[]
 
     def push(self,added):
-        if len(self.stack)< self.max:
+        if self.size()< self.max:
             self.stack.append(added)
         else:
-            print("Stack is full... :(")   
+            print("Stack is full... :(") 
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop(-1)
+        else:
+            print("stack is empty..:(") 
+    def show(self):
+        print(self.stack)
+    def is_empty (self):
+        if len(self.stack)==0:
+           return True
+        else:
+           return False
+    def size(self):
+        return len(self.stack)
+    def top(self):
+        return self.stack[-1]
+       
+
+
+my_stack=Stack(3)
+my_stack.show()
+my_stack.push(4)
+my_stack.show()
+my_stack.push(3)
+my_stack.show()
+print(my_stack.top())
+my_stack.push(2)
+my_stack.show()
+my_stack.push(1)
+my_stack.show()
+for i in range(4):
+    i=my_stack.pop()
+    print(i)
+    my_stack.show()
+
+
+ 
         
