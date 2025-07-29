0f61e6eae8aef0a2083ead866574f97aa1d712c6
D.P <dhanyata2512@gmail.com>
2025-07-29 18:41:54 +0100

    Update tree.py

--- tree.py
+++ tree.py
@@ -4,10 +4,37 @@ class Node():
         self.l_child=None
         self.r_child=None
 
+def inorder_treversal(root):
+    if root.l_child is not None: # There is a left child present
+        inorder_treversal(root.l_child)
+    print(root.key)
+    if root.r_child is not None:
+        inorder_treversal(root.r_child)
+
+def preorder_treversal(root):
+    print(root.key)
+    if root.l_child is not None:
+        preorder_treversal(root.l_child)
+    if root.r_child is not None:
+        preorder_treversal(root.r_child)
+def postorder_treversal(root):
+    if root.l_child is not None:
+        postorder_treversal(root.l_child)
+    if root.r_child is not None:
+        postorder_treversal(root.r_child)
+    print(root.key)            
+
 root=Node(2)
 root.l_child=Node(3)
 root.r_child=Node(13)
 root.l_child.l_child=Node(4)
 root.l_child.r_child=Node(7)
 root.r_child.l_child=Node(6)
-root.r_child.r_child=Node(14)        
+root.r_child.r_child=Node(14)
+
+inorder_treversal(root)
+print("---------")
+preorder_treversal(root)
+print("---------")
+postorder_treversal(root)
+
