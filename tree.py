8366b5f24ef03be3097007534a80c663cb1ba2de
D.P <dhanyata2512@gmail.com>
2025-08-05 18:58:22 +0100

    Update tree.py

--- tree.py
+++ tree.py
@@ -4,6 +4,8 @@ class Node():
         self.l_child=None
         self.r_child=None
 
+question=int(input("which treversal would you like to do on your tree: \n 1- inorder treversal \n 2- preorder treversal \n 3- postorder trversal \n"))
+
 def inorder_treversal(root):
     if root.l_child is not None: # There is a left child present
         inorder_treversal(root.l_child)
@@ -17,12 +19,14 @@ def preorder_treversal(root):
         preorder_treversal(root.l_child)
     if root.r_child is not None:
         preorder_treversal(root.r_child)
+
 def postorder_treversal(root):
     if root.l_child is not None:
         postorder_treversal(root.l_child)
     if root.r_child is not None:
         postorder_treversal(root.r_child)
-    print(root.key)            
+    print(root.key)
+
 
 root=Node(2)
 root.l_child=Node(3)
@@ -32,9 +36,15 @@ root.l_child.r_child=Node(7)
 root.r_child.l_child=Node(6)
 root.r_child.r_child=Node(14)
 
-inorder_treversal(root)
-print("---------")
-preorder_treversal(root)
-print("---------")
-postorder_treversal(root)
+
+if question == 1:
+    inorder_treversal(root)
+
+elif question == 2:
+    preorder_treversal(root)
+   
+elif question == 3:
+    postorder_treversal(root)  
+else:
+    print("The number you have entered is not an known treversal...\n Please try again later\n :(")   
 
