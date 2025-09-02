134d662cfda1f6bcafac92fab97c4f6866cafa01
D.P <dhanyata2512@gmail.com>
2025-09-02 18:48:29 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import queue_class
+import StackDS
 class Graph:
     def __init__(self,nodes):
         self.nodes=nodes
@@ -21,16 +22,31 @@ class Graph:
                 if i not in visited:
                     queue.enqueue(i)
                     visited.append(i)
-        print(visited)            
+        print(visited)
+
+    def DFS (self,start):
+        visted=[]  
+        stack=StackDS.Stack(self.nodes)
+        stack.push(start)
+        while not stack.is_empty() :
+            item=stack.pop()
+            visted.append(item)
+            for i in self.adjacency_list[item]:
+                if i not in visted and not stack.is_in(i):
+                    stack.push(i)
+        print(visted)            
+
+            
 
 
 g=Graph(6)  
 g.add_edge(0,1)
 g.add_edge(0,2)
 g.add_edge(0,4)
-g.add_edge(1,3)
+#g.add_edge(1,3)
 g.add_edge(1,5) 
 g.add_edge(2,3)
-g.add_edge(2,4)
-g.add_edge(3,5)
+#g.add_edge(2,4)
+#g.add_edge(3,5)
 g.BFS(0)
+g.DFS(1)
